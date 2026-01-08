0eb4d275d604ad96c8f89c64249fad3e119627d1
RUHUL AMIN <ruhulamiinn1316@gmail.com>
2026-01-08 19:23:17 +0600

    update

--- ec/settings.py
+++ ec/settings.py
@@ -9,32 +9,35 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'sharatacharjee6@gmail.com'
-EMAIL_HOST_PASSWORD = 'iyxrzfhdjoxvguhq'  # ⚠️ no spaces
-DEFAULT_FROM_EMAIL = 'Daily Dairy Shop <sharatacharjee6@gmail.com>'
-
-
-
-
 # ---------------- BASE DIR ----------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ---------------- SECURITY ----------------
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4^hblj$3@q-n3mp%vmvy&367_c*@jqj$bo7@25wq_6mkd9gu&f')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-4^hblj$3@q-n3mp%vmvy&367_c*@jqj$bo7@25wq_6mkd9gu&f'
+)
+
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
-    'dailydairyshop-3.onrender.com',
     'dairy-shop-bd.onrender.com',
     '127.0.0.1',
     'localhost',
-    '.onrender.com'
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://dairy-shop-bd.onrender.com',
+]
+
+# ---------------- EMAIL ----------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ruhulaminn1316@gmail.com'
+EMAIL_HOST_PASSWORD = 'iyxrzfhdjoxvguhq'
+DEFAULT_FROM_EMAIL = 'Daily Dairy Shop <ruhulaminn1316@gmail.com>'
 
 # ---------------- LOGIN ----------------
 LOGIN_URL = '/accounts/login/'
@@ -58,17 +61,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',   # ✅ REQUIRED
+    'django.contrib.sites',
 
     # allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-   
 
-
-    # your apps
+    # local apps
     'app',
     'adminpanel',
 
@@ -79,7 +80,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-# ---------------- AUTHENTICATION ----------------
+# ---------------- AUTH BACKENDS ----------------
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
@@ -109,7 +110,6 @@ cloudinary.config(
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # ---------------- MIDDLEWARE ----------------
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -120,14 +120,12 @@ MIDDLEWARE = [
 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 
-    # ✅ THIS LINE FIXES YOUR ERROR
+    # REQUIRED for allauth
     'allauth.account.middleware.AccountMiddleware',
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-
 
 # ---------------- URL / TEMPLATE ----------------
 ROOT_URLCONF = 'ec.urls'
@@ -181,12 +179,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# ---------------- PAYMENT ----------------
-BKASH_APP_KEY = os.environ.get('BKASH_APP_KEY', '')
-BKASH_APP_SECRET = os.environ.get('BKASH_APP_SECRET', '')
-BKASH_BASE_URL = os.environ.get('BKASH_BASE_URL', 'https://tokenized-sandbox.bkash.com')
-
-NAGAD_MERCHANT_ID = os.environ.get('NAGAD_MERCHANT_ID', '')
-NAGAD_MERCHANT_PASS = os.environ.get('NAGAD_MERCHANT_PASS', '')
-NAGAD_BASE_URL = os.environ.get('NAGAD_BASE_URL', 'https://sandbox.mynagad.com')
